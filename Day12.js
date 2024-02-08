6aadcd5d2b31d01c11fab9b8b05f7aef71d0d1f4
Sham Machale <sham.machale0102@gmail.com>
2024-02-08 18:56:22 +0530

    Day 12

--- Day12.js
+++ Day12.js
@@ -9,9 +9,26 @@ for(frt of fruits){
     document.getElementById("msg").innerHTML += frt + "<br>"
 }
 
+
+
+
+// function change(){
+//     var h1 = document.querySelector(".head")
+    
+//     h1.innerHTML = "Learning DOM... "
+//     h1.style.backgroundColor = "yellow"
+// }
+
+
 function change(){
     var h1 = document.querySelector(".head")
-    
+if (h1.innerHTML == "Day 12 - Introduction to DOM") {
     h1.innerHTML = "Learning DOM... "
-    h1.style.backgroundColor = "orange"
+    h1.style.backgroundColor = "red"
+    
+} else {
+    h1.innerHTML = "Day 12 - Introduction to DOM"
+    h1.style.backgroundColor = "yellow"
+}
+   
 }
