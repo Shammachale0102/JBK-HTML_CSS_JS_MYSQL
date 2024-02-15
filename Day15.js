c07568d2722140d8af6a4cda16c2c4f724b0ca08
Sham Machale <sham.machale0102@gmail.com>
2024-02-15 22:07:32 +0530

    Day 15

--- Day15.js
+++ Day15.js
@@ -1,15 +1,57 @@
-function login() {
-  window.open("login.html")
+
+function login(){
+    window.open("login.html")
 }
 
-function checkemail(){
+function checkmail(){
     var e = document.getElementById("uname").value
 
-    var emailRegex = /\S+@\S+.\S+/.test(e)
+    var emailRegex = /\S+@\S+\.\S+/.test(e);
 
     if (emailRegex) {
-        document.getElementById("echeck").innerHTML ="Email is correct"
-    } else{
-        document.getElementById("echeck").innerHTML ="Email is Incorrect"
+        document.getElementById("echeck").innerHTML="Email is correct"
+        return true;
+
+    } else {
+        document.getElementById("echeck").innerHTML="Email is Incorrect"
+        return false;
+    }
+}
+
+function checkpass(){
+    var p = document.getElementById("pass").value
+
+    var passRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{8,}/.test(p);
+
+    if (passRegex) {
+        document.getElementById("pmsg").innerHTML="Password is strong"
+        return true;
+
+    } else {
+        document.getElementById("pmsg").innerHTML="Password is weak"
+        return false;
+    }
+}
+
+function checkmob(){
+    var m = document.getElementById("mob").value
+
+    var mobRegex = /^\d{10}$/.test(mob);
+
+    if (mobRegex) {
+        document.getElementById("mmsg").innerHTML="Mobile number is valid"
+        return true;
+
+    } else {
+        document.getElementById("mmsg").innerHTML="Mobile number Invalid"
+        return false;
+    }
+}
+
+function login(){
+    if (checkmail() && checkpass() && checkmob()){
+        window.open("login.html")
+    }else{
+        document.getElementById("msg").innerHTML = "Invalid Id or Password"
     }
 }
---
+++ Day16.js
@@ -0,0 +1,9 @@
+
+let c = 0
+
+var Images = ["Images/IMG_20230723_124317.jpg" , "Images/IMG_20230723_124317.jpg" ,"Images/IMG_20230723_124317.jpg" ]
+
+function changeimg(){
+    c (c+1)
+    document.getElementById("changeimage").innerHTML = <img src='"+Images[c]+"'></img>
+}
