8878043d1023643abbc3996d8dddf9d75f2a50aa
Sham Machale <sham.machale0102@gmail.com>
2024-02-16 14:12:26 +0530

    Day 16

--- Day16.js
+++ Day16.js
@@ -3,7 +3,7 @@ let c = 0
 
 var Images = ["Images/IMG_20230723_124317.jpg" , "Images/IMG_20230723_124317.jpg" ,"Images/IMG_20230723_124317.jpg" ]
 
-function changeimg(){
+function changeimage(){
     c (c+1)
     document.getElementById("changeimage").innerHTML = <img src='"+Images[c]+"'></img>
 }
