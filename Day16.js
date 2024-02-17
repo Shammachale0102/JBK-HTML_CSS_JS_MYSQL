ad35882fdd6455f97c87bebeb933febf99a11981
Sham Machale <sham.machale0102@gmail.com>
2024-02-17 15:27:19 +0530

    Day 16

--- Day16.js
+++ Day16.js
@@ -1,9 +1,9 @@
 
 let c = 0
 
-var Images = ["Images/IMG_20230723_124317.jpg" , "Images/IMG_20230723_124317.jpg" ,"Images/IMG_20230723_124317.jpg" ]
+var Images = ["Images/IMG_20230723_124317.jpg" , "Images/IMG_20230723_124545.jpg" ,"Images/IMG_20230723_180841.jpg" ]
 
 function changeimage(){
-    c (c+1)
+    c (c+1) % Images.length ;
     document.getElementById("changeimage").innerHTML = <img src='"+Images[c]+"'></img>
 }
