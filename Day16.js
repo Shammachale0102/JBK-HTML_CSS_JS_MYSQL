77d149be1e330c2230dea9b3016a81c0250ca67e
Sham Machale <sham.machale0102@gmail.com>
2024-02-20 09:52:15 +0530

    Day 16

--- Day16.js
+++ Day16.js
@@ -1,9 +1,9 @@
 
 let c = 0
 
-var Images = ["Images/IMG_20230723_124317.jpg" , "Images/IMG_20230723_124545.jpg" ,"Images/IMG_20230723_180841.jpg" ]
+var images = ["Images/IMG_20230723_124317.jpg" , "Images/IMG_20230723_124545.jpg" ,"Images/IMG_20230723_180841.jpg" ]
 
-function changeimage(){
-    c (c+1) % Images.length ;
-    document.getElementById("changeimage").innerHTML = <img src='"+Images[c]+"'></img>
+function changeimg(){
+    c = (c+1) % images.length;
+    document.getElementById("changeimage").innerHTML= "<img src='"+images[c]+"'>"
 }
